b70c0801074dc2cf43344ec8c2497ed339080293
Samuel Ige <samuelige.iyanu@gmail.com>
2023-12-30 00:35:42 +0100

    initial application setup

---
+++ theme.js
@@ -0,0 +1,58 @@
+module.exports = {
+    fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+    },
+    colors: {
+        gray: {
+          25: "#F4F7F7",
+          50: "#F9FAFC",
+          100: "#D9D9D9",
+          200: "#00000099",
+        },
+        success: {
+          25: "#2c676324",
+          100: "#2C6763",
+        },
+        white: "#FFFFFF",
+        black: {
+            100: "#000000",
+            200: "#2B2B2B"
+        },
+    },
+    backgroundColor: {
+    gray: {
+        25: "#F4F7F7",
+        50: "#F9FAFC",
+        100: "#D9D9D9",
+        200: "#00000099",
+    },
+    success: {
+        25: "#2c676324",
+        100: "#2C6763",
+    },
+    white: "#FFFFFF",
+    black: {
+        100: "#000000",
+        200: "#2B2B2B"
+    },
+    },
+    borderColor: {
+    gray: {
+        25: "#F4F7F7",
+        50: "#F9FAFC",
+        100: "#D9D9D9",
+        200: "#00000099",
+    },
+    success: {
+        25: "#2c676324",
+        100: "#2C6763",
+    },
+    white: "#FFFFFF",
+    black: {
+        100: "#000000",
+        200: "#2B2B2B"
+    },
+    },
+    backgroundImage: {},
+    boxShadow: {}
+}
